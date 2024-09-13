3e470e49c44832905f7624af354d5381de5c4b29
Vân Anh <89188722+yw07761@users.noreply.github.com>
2024-09-13 07:38:29 +0700

    Create example2.js
    increment by 1 a number in array

---
+++ example2.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { View, Text, Button } from 'react-native'
+
+ export default function App() {
+     const [numbers, setNumbers] = useState([5,6,7])
+     return (
+        <View>
+           <Button
+               title="Increase number in array"
+                  onPress={() => {
+                     setNumbers(numbers.map(num => num + 1)) // Tang moi phan tu len 1
+                  }}/>
+        {numbers.map((numbers, index) => (
+        <Text style={{ fontSize: 24 }} key={index}>
+          {numbers}
+        </Text>
+      ))}
+    </View>
+   )
+ }
